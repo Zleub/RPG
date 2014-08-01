c1181c6df00137997ce254438c22d7fc68947237
Arnaud Debray <debray.arnaud@gmail.com>
2014-08-01 17:09:55 +0200

    save up

--- src/RPG/manage_biome_list.c
+++ src/RPG/manage_biome_list.c
@@ -12,65 +12,6 @@ t_biome_list		*create_biome_list(void)
 	return (new);
 }
 
-int						get_lvl(char *line)
-{
-	int			i;
-
-			i = 0;
-		while (line[i] == '-')
-			i += 1;
-		return (i);
-}
-
-t_biome_list			*get_prev(t_biome_list *head, int lvl)
-{
-	while (head->next)
-		head = head->next;
-	if (lvl && head->top)
-		return (get_prev(head->top, lvl - 1));
-	else
-		return (head);
-}
-
-t_biome_list			*fill_biome_list(int fd)
-{
-	t_biome_list		*head;
-	char		*line;
-	int			i;
-
-	get_next_line(fd, &line);
-	head = create_biome_list();
-	head->biome = ft_strdup(line);
-
-	t_biome_list *tmp;
-	i = 0;
-
-	while (get_next_line(fd, &line))
-	{
-		i = get_lvl(line);
-		tmp = get_prev(head, i);
-
-		ft_printf("%d, %p: %s\n", i, tmp, tmp->biome);
-	}
-	(void)tmp;
-	return (head);
-}
-
-t_biome_list		*new_biome_list(void)
-{
-	t_biome_list		*head;
-	int					fd;
-
-	if (access("data/biome_list", R_OK) == -1)
-		return (NULL);
-	fd = open("data/biome_list", O_RDONLY);
-
-	head = fill_biome_list(fd);
-
-	close(fd);
-	return (head);
-}
-
 void				print_tab(int nbr)
 {
 	while (nbr)
@@ -90,6 +31,84 @@ void				print_biome_list(t_biome_list *head, int level)
 	ft_printf("head->biome: \'%s\'\n", head->biome);
 	print_biome_list(head->top, level + 1);
 	print_biome_list(head->next, level);
+}
+
+int						get_lvl(char *line)
+{
+	int			i;
+
+	i = 0;
+	while (line[i] == '-')
+		i += 1;
+	return (i);
+}
+
+void					insert_next_line(int fd, t_biome_list *head, char *line)
+{
+	int				cmp;
+	int				i;
+
+	ft_printf("1 test de merde: %p\n", head);
+	(void)fd;
+	i = get_lvl(line);
+	cmp = i;
+
+	while (head->next)
+	{
+		head = head->next;
+	}
+		ft_printf("debuf: %p\n", head);
+
+		ft_printf("debuf: %p\n", head->top);
+	while (head->top && cmp)
+	{
+		head = head->top;
+		while (head->next)
+			head = head->next;
+		cmp--;
+	}
+	ft_printf("2 test de merde: %d %p\n", i, head);
+	if (i != 0)
+	{
+		head->top = create_biome_list();
+		head->top->biome = ft_strdup(line);
+	}
+	else
+	{
+		head->next = create_biome_list();
+		head->next->biome = ft_strdup(line);
+	}
+}
+
+t_biome_list			*fill_biome_list(int fd)
+{
+	t_biome_list		*head;
+	char		*line;
+	// int			i;
+
+	get_next_line(fd, &line);
+	head = create_biome_list();
+	head->biome = ft_strdup(line);
+
+	while (get_next_line(fd, &line))
+		insert_next_line(fd, head, line);
+
+	return (head);
+}
+
+t_biome_list		*new_biome_list(void)
+{
+	t_biome_list		*head;
+	int					fd;
+
+	if (access("data/biome_list", R_OK) == -1)
+		return (NULL);
+	fd = open("data/biome_list", O_RDONLY);
+
+	head = fill_biome_list(fd);
+
+	close(fd);
+	return (head);
 }
 
 t_biome_list		*manage_biome_list(int macro)
