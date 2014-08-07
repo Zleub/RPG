91de7fc5ec77a419ffbdbd77bd159e8adada4432
Arnaud Debray <debray.arnaud@gmail.com>
2014-08-07 19:34:04 +0200

    event workout

--- src/RPG/game_event.c
+++ src/RPG/game_event.c
@@ -6,7 +6,7 @@
 /*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/08/05 21:12:19 by adebray           #+#    #+#             */
-/*   Updated: 2014/08/07 19:10:30 by adebray          ###   ########.fr       */
+/*   Updated: 2014/08/07 19:33:30 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void		event_vendredi(t_heros *heros)
 
 	if (heros->level >= 5)
 	{
+		dprintf(2, "Vendredi: %d %% 100 : %d\n", tmp, tmp % 100);
 		if (!tmp || tmp < 100)
 			tmp = (int)time(NULL);
 		else if ((tmp % 100) == 0)
@@ -40,14 +41,25 @@ void		event_calendar(t_heros *heros)
 
 void		event_rand_char(t_heros *heros)
 {
+	static int	check;
 	int		tmp;
 
+	tmp = 0;
 	if (heros->level >= 10 && !ft_strcmp(heros->state->event, "spare"))
 	{
+		dprintf(2, "Rand_char(%d): %d %% 100 : %d\n", check, tmp, tmp % 100);
 		tmp = (int)time(NULL);
-		if ((tmp % 100) == 0)
+		if ((tmp % 100) == 0 && check == 0)
+		{
 			manage_char(NEW, ft_itoa(tmp));
+			check = 1;
+		}
+		else if ((tmp % 100) == 25 && check == 1)
+		{
+			check = 0;
+		}
 		else
 			tmp /= 10;
+
 	}
 }
